ab4436a7ae7c763b2a59bac24e5da02b5415a7dc
yuanjiatan <jiatan.yuan@mail.utoronto.ca>
2022-10-09 19:22:54 -0400

    3

--- src/main/java/BagMain.java
+++ src/main/java/BagMain.java
@@ -18,7 +18,7 @@ class BagMain {
         // TODO: Implement this.
         for (Bag bag : bags){
             bag.enhance();
-            if (double_enhance_handbags && bag instanceof  HandBag){
+            if (double_enhance_handbags & bag instanceof  HandBag){
                 bag.enhance();
             }
         }
